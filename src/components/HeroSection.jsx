cb9eec3f70d201f1c31779faca2475b62b018db1
Nao note <naocaroling@gmail.com>
2025-04-07 15:56:53 +0500

    hero section jsx and css changed lil bit(also adaptivity)

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,14 +2,12 @@ import "../styles/HeroSection.css";
 
 function HeroSection() {
   return (
-    <div className="hero">
-      <div className="hero-content-wrapper">
-        <div className="hero-content">
-          <h1>Shielded Networks</h1>
-          <p>Лучший щит от кибер атак на вашу компанию</p>
-        </div>
+    <section className="hero">
+      <div className="hero-content">
+        <h1>Shielded Networks</h1>
+        <p>Лучший щит от кибер атак на вашу компанию</p>
       </div>
-    </div>
+    </section>
   );
 }
 
